fe6cc9928bbc3d62d879db2585c189ea66cdeaa1
Chartreuse310 <yoyflying@163.com>
2025-03-15 22:44:01 +0800

    initialised basic settings (trial 02)

--- main.ts
+++ main.ts
@@ -17,16 +17,16 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'MemoNote', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('This is a MemoNote!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('MemoNote working.');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -36,13 +36,13 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		// 替换选中文字
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'sample-replace-text',
+			name: 'Replace text (sample)',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				editor.replaceSelection('ヾ(≧▽≦*)o');
 			}
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
